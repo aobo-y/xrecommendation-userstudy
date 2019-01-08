60024fe4d6f0d81bd7a79ae27bdf64eb097897b5
Aobo Yang <yangaobo@gmail.com>
2019-01-08 16:08:53 -0500

    update question id to linear idx

--- src/Main/QuestionList.js
+++ src/Main/QuestionList.js
@@ -19,12 +19,12 @@ class QuestionList extends Component {
         itemLayout="vertical"
         size="large"
         dataSource={questions}
-        renderItem={question => (
+        renderItem={(question, idx) => (
           <List.Item key={question.id}>
             {
               question.submitted ?
-                <QuestionItem id={question.id} feature={question.feature} submitted /> :
-                <QuestionItem id={question.id} feature={question.feature} onSubmit={v => onSubmit(question.id, v)} />
+                <QuestionItem id={idx + 1} feature={question.feature} submitted /> :
+                <QuestionItem id={idx + 1} feature={question.feature} onSubmit={v => onSubmit(question.id, v)} />
             }
           </List.Item>
         )}
