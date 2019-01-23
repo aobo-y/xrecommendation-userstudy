600ad8e9553092b289fa3d178eb43dff138ca664
Aobo Yang <yangaobo@gmail.com>
2019-01-23 12:12:55 -0500

    update survey

--- src/lib/surveys.js
+++ src/lib/surveys.js
@@ -1,19 +1,19 @@
 const surveys = {
   amazon: [
-    '',
-    '',
-    '',
-    '',
-    '',
-    ''
+    'http://koudaigou.net/web/formview/5c4881f3fc918f0bcc2c2f38',
+    'http://yingkebao.top/web/formview/5c488213fc918f0bcc2c2f52',
+    'http://yingkebao.top/web/formview/5c488215fc918f0bcc2c2f54',
+    'http://yingkebao.top/web/formview/5c488217fc918f0bcc2c2f58',
+    'http://yingkebao.top/web/formview/5c48821bfc918f0bcc2c2f5a',
+    'http://koudaigou.net/web/formview/5c488228fc918f0bcc2c2f66'
   ],
   yelp: [
-    '',
-    '',
-    '',
-    '',
-    '',
-    ''
+    'http://yingkebao.top/web/formview/5c486b28fc918f0bcc2c113d',
+    'http://koudaigou.net/web/formview/5c488188fc918f0bcc2c2ecf',
+    'http://koudaigou.net/web/formview/5c48818bfc918f0bcc2c2ed2',
+    'http://koudaigou.net/web/formview/5c48818dfc918f0bcc2c2ed8',
+    'http://yingkebao.top/web/formview/5c488190fc918f0bcc2c2eda',
+    'http://yingkebao.top/web/formview/5c4881a2fc918f0bcc2c2eed'
   ]
 }
 
